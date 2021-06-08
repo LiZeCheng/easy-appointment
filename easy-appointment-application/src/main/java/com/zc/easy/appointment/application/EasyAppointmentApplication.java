c449246ff4b593032a9b922f2348f84bc275c7d5
LiZc <20790866@qq.com>
2021-06-08 09:41:38 +0800

    init

---
+++ easy-appointment-application/src/main/java/com/zc/easy/appointment/application/EasyAppointmentApplication.java
@@ -0,0 +1,15 @@
+package com.zc.easy.appointment.application;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author lizecheng
+ * @date 2021/6/8 9:34
+ */
+@SpringBootApplication
+public class EasyAppointmentApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(EasyAppointmentApplication.class, args);
+    }
+}
---
+++ easy-appointment-application/src/main/java/com/zc/easy/appointment/application/controller/LoginController.java
@@ -0,0 +1,17 @@
+package com.zc.easy.appointment.application.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @author lizecheng
+ * @date 2021/6/8 9:38
+ */
+@RestController
+public class LoginController {
+
+    @GetMapping("/hi")
+    public String hi(){
+        return "hello world";
+    }
+}
